9eb659fd79afe26308936aa8d5e13b147d8a6c96
breno.polanski <breno.polanski@gmail.com>
2014-02-04 23:52:41 -0200

    Added methods: 'onKeyUp' and 'onKeyDown'

--- src/Input/Keyboard/js/Keyboard.js
+++ src/Input/Keyboard/js/Keyboard.js
@@ -5,10 +5,20 @@ define(['../../../../Cactus'],
 		var keyPressed = function (key) {
 			key = Array.prototype.slice.call(arguments);
 			return C.pressed(key[0].toString());
-		};
+		},
+
+		onKeyUp = function (key, callback) {
+			return C.on_keyup(key, callback);
+		},
+
+		onKeyDown = function (key, callback) {
+			return C.on_keydown(key, callback);
+		};		
 
 		return {
-			keyPressed: keyPressed
+			keyPressed: keyPressed,
+			onKeyUp: onKeyUp,
+			onKeyDown: onKeyDown
 		}
 	}
 );
