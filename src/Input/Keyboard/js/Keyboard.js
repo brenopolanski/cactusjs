0587733962f0e5615549ce2a0f3622c2ee4eeffc
Breno Polanski <breno.polanski@gmail.com>
2014-04-03 00:25:44 -0300

    Update Keyboard.js

--- src/Input/Keyboard/js/Keyboard.js
+++ src/Input/Keyboard/js/Keyboard.js
@@ -1,68 +1,67 @@
-define(['Cactus'], 
-    /**
-    * The Keyboard object allows projects to respond to keyboard input
-    * @class Keyboard
-    */
-    function (C) {
+/**
+* The Keyboard object allows projects to respond to keyboard input.
+* @class Keyboard
+*/
+define(['Cactus'], function(C) {
 		'use strict';
 
         /**
-        * Check if array keys are pressed
+        * Check if array keys are pressed.
         * 
         * @method arrayKeysPressed
         * @param {Array} keys Array of key Codes letters or words
-        * @return (Boolean) Boolean if keys pressed
+        * @return {Boolean} True if keys pressed
         */
-        var arrayKeysPressed = function (keys) {
+        var arrayKeysPressed = function(keys) {
             keys = keys.join(' ');
             return C.pressed(keys.toString());
 		},
 
         /**
-        * Check if key are pressed
+        * Check if key are pressed.
         * 
         * @method keyPressed
         * @param {String} key Key Codes letters or words
-        * @return (Boolean) Boolean if key pressed
+        * @return {Boolean} True if key pressed
         */
-	    keyPressed = function (key) {
+	    keyPressed = function(key) {
 			key = Array.prototype.slice.call(arguments);
 			return C.pressed(key[0].toString());
 	    },
 
         /**
-        * Check if key are pressed without repeat
+        * Check if key are pressed without repeat.
         * 
         * @method keyPressedWithoutRepeat
         * @param {String} key Key Codes letters or words
-        * @return (Boolean) Boolean if key pressed
+        * @return {Boolean} True if key pressed
         */
-	    keyPressedWithoutRepeat = function (key) {
+	    keyPressedWithoutRepeat = function(key) {
 	        key = Array.prototype.slice.call(arguments);
 	        return C.pressedWithoutRepeat(key[0].toString());
 	    },
 
         /**
-        * Sets up a callback when a key (or array of keys) to call when it's released
+        * Sets up a callback when a key (or array of keys) to call when it's released.
         * 
         * @method onKeyUp
         * @param {String or Array} key Key Codes letters or words
         * @param {Function} callback Function that is returned
-        * @return (Function) Start function
+        * @return {Function} Start function
         */
-        onKeyUp = function (key, callback) {
+        onKeyUp = function(key, callback) {
 			return C.on_keyup(key, callback);
 		},
 
         /**
-        * Sets up a callback for a key (or array of keys) to call when it's pressed down
+        * Sets up a callback for a key (or array of keys) to call when it's pressed down.
         * 
         * @method onKeyDown
         * @param {String or Array} key Key Codes letters or words
         * @param {Function} callback Function that is returned
-        * @return (Function) Start function
+        * @return {Function} Start function
         */
-		onKeyDown = function (key, callback) {
+		onKeyDown = function(key, callback) {
 			return C.on_keydown(key, callback);
 		};		
 
